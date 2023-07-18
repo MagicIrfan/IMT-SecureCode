4a4b31b70514690b9fb3b9f4a278bacafc099ae4
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2023-07-18 11:10:55 +0200

    bonjour

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 import socket
 
 # Configuration du serveur
@@ -31,24 +32,26 @@ def send_request(request):
 
 
 def get_time():
-    format_string = format_entry.get()
-    print(format_string)
-    if format_string:
-        request = f"GET_TIME:{format_string}"
-        response = send_request(request)
-        print(response)
-        if response:
-            time_label.config(text="Heure actuelle : " + response)
+    format_string = "%Y-%m-%d %H:%M:%S"
+    request = f"GET_TIME:{format_string}"
+    response = send_request(request)
+    if response:
+        time_label.config(text="Heure actuelle : " + response)
 
 
 def set_time():
-    new_time = new_time_entry.get()
-    if new_time:
-        request = f"SET_TIME:{new_time}"
-        response = send_request(request)
-        print(response)
-        if response:
-            messagebox.showinfo("Changement d'heure", response)
+    year = year_var.get()
+    month = month_var.get()
+    day = day_var.get()
+    hour = hour_var.get()
+    minute = minute_var.get()
+    second = second_var.get()
+
+    new_time = f"{year}-{month}-{day}-12-12-12"
+    request = f"SET_TIME:{new_time}"
+    response = send_request(request)
+    if response:
+        messagebox.showinfo("Changement d'heure", response)
 
 
 if __name__ == '__main__':
@@ -57,27 +60,60 @@ if __name__ == '__main__':
     root.title("Horloge réseau")
 
     # Création des widgets
-    format_label = tk.Label(root, text="Format de l'heure (ex: %Y-%m-%d %H:%M:%S) :")
-    format_entry = tk.Entry(root, width=30)
-    get_time_button = tk.Button(root, text="Obtenir l'heure actuelle", command=get_time)
+    year_var = tk.StringVar()
+    month_var = tk.StringVar()
+    day_var = tk.StringVar()
+    hour_var = tk.StringVar()
+    minute_var = tk.StringVar()
+    second_var = tk.StringVar()
 
-    new_time_label = tk.Label(root, text="Nouvelle date et heure (ex: 2023-07-17 12:34:56) :")
-    new_time_entry = tk.Entry(root, width=30)
+    year_label = tk.Label(root, text="Année :")
+    year_entry = ttk.Combobox(root, textvariable=year_var, values=list(range(2000, 2101)))
+
+    month_label = tk.Label(root, text="Mois :")
+    month_entry = ttk.Combobox(root, textvariable=month_var, values=list(range(1, 13)))
+
+    day_label = tk.Label(root, text="Jour :")
+    day_entry = ttk.Combobox(root, textvariable=day_var, values=list(range(1, 32)))
+
+    hour_label = tk.Label(root, text="Heure :")
+    hour_entry = ttk.Combobox(root, textvariable=hour_var, values=list(range(24)))
+
+    minute_label = tk.Label(root, text="Minutes :")
+    minute_entry = ttk.Combobox(root, textvariable=minute_var, values=list(range(60)))
+
+    second_label = tk.Label(root, text="Secondes :")
+    second_entry = ttk.Combobox(root, textvariable=second_var, values=list(range(60)))
+
+    get_time_button = tk.Button(root, text="Obtenir l'heure actuelle", command=get_time)
     set_time_button = tk.Button(root, text="Définir la date et l'heure système", command=set_time)
 
     time_label = tk.Label(root, text="Heure actuelle :")
     time_label.config(font=("Arial", 14, "bold"))
 
     # Placement des widgets dans la fenêtre
-    format_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
-    format_entry.grid(row=0, column=1, padx=10, pady=5)
-    get_time_button.grid(row=0, column=2, padx=10, pady=5)
+    year_label.grid(row=0, column=0, padx=10, pady=5)
+    year_entry.grid(row=0, column=1, padx=10, pady=5)
 
-    new_time_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
-    new_time_entry.grid(row=1, column=1, padx=10, pady=5)
-    set_time_button.grid(row=1, column=2, padx=10, pady=5)
+    month_label.grid(row=0, column=2, padx=10, pady=5)
+    month_entry.grid(row=0, column=3, padx=10, pady=5)
 
-    time_label.grid(row=2, column=0, columnspan=3, padx=10, pady=5)
+    day_label.grid(row=0, column=4, padx=10, pady=5)
+    day_entry.grid(row=0, column=5, padx=10, pady=5)
+
+    hour_label.grid(row=1, column=0, padx=10, pady=5)
+    hour_entry.grid(row=1, column=1, padx=10, pady=5)
+
+    minute_label.grid(row=1, column=2, padx=10, pady=5)
+    minute_entry.grid(row=1, column=3, padx=10, pady=5)
+
+    second_label.grid(row=1, column=4, padx=10, pady=5)
+    second_entry.grid(row=1, column=5, padx=10, pady=5)
+
+    get_time_button.grid(row=2, column=0, columnspan=3, padx=10, pady=5)
+    set_time_button.grid(row=2, column=3, columnspan=3, padx=10, pady=5)
+
+    time_label.grid(row=3, column=0, columnspan=6, padx=10, pady=5)
 
     # Boucle principale de la fenêtre
     root.mainloop()
--- server/server.py
+++ server/server.py
@@ -1,5 +1,25 @@
+import ctypes
 import socketserver
 import datetime
+from datetime import datetime
+import platform
+
+import pywintypes
+import win32api
+
+
+# Define the SYSTEMTIME structure manually
+class SYSTEMTIME(ctypes.Structure):
+    _fields_ = [
+        ("wYear", ctypes.c_uint16),
+        ("wMonth", ctypes.c_uint16),
+        ("wDayOfWeek", ctypes.c_uint16),
+        ("wDay", ctypes.c_uint16),
+        ("wHour", ctypes.c_uint16),
+        ("wMinute", ctypes.c_uint16),
+        ("wSecond", ctypes.c_uint16),
+        ("wMilliseconds", ctypes.c_uint16),
+    ]
 
 
 # Classe pour gérer les demandes de clients distants
@@ -23,12 +43,56 @@ class NetworkClockRequestHandler(socketserver.BaseRequestHandler):
         self.request.send(formatted_time.encode())
 
     def handle_set_time(self, new_time):
-        try:
+        """try:
             datetime.datetime.strptime(new_time, "%Y-%m-%d %H:%M:%S")
             # Code pour définir la date et l'heure système ici
             self.request.send("Date and time have been set successfully.".encode())
         except ValueError:
-            self.request.send("Invalid date and time format. Please use the format: YYYY-MM-DD HH:MM:SS".encode())
+            self.request.send("Invalid date and time format. Please use the format: YYYY-MM-DD HH:MM:SS".encode())"""
+        try:
+            # datetime.datetime.strptime(new_time, "%Y-%m-%d %H:%M:%S")
+            print(new_time)
+            # Check the platform and call the appropriate function
+            platform_name = platform.system()
+            if platform_name == 'Windows':
+                return self.set_system_time_windows(new_time)
+            else:
+                return self.set_system_time_unix(new_time)
+        except ValueError:
+            print("Invalid date and time format.")
+            return False
+        # Function to set the system time on Windows
+
+    def set_system_time_windows(self, new_time):
+        try:
+            # Parse new_time and convert it to a SYSTEMTIME structure
+            year, month, day, hour, minute, second = map(int, new_time.split("-"))
+            system_time = SYSTEMTIME(year, month, day, hour, minute, second, 0)
+
+            # Call the SetSystemTime function to set the system time
+            if win32api.SetSystemTime(system_time.wYear, system_time.wMonth, 0,
+                                      system_time.wDay, system_time.wHour,
+                                      system_time.wMinute, system_time.wSecond,
+                                      system_time.wMilliseconds):
+                self.request.send("oui".encode())
+                return True
+            else:
+                self.request.send("non".encode())
+                return False
+        except Exception as e:
+            print("Error:", e)
+            return False
+
+    # Function to set the system time on non-Windows systems (Linux, macOS, etc.)
+    def set_system_time_unix(self, new_time):
+        try:
+            # Use the 'date' command to set the system time
+            import subprocess
+            subprocess.run(['date', '-s', new_time], check=True)
+            return True
+        except Exception as e:
+            print("Error:", e)
+            return False
 
 
 if __name__ == '__main__':
