473ebd0901aad56863ef4fc4c72e042548be4654
MagicIrfan <irfan.bouhenaf@etu.mines-ales.fr>
2023-07-25 16:53:03 +0200

    receive_data correct

--- utils/socket_utils.py
+++ utils/socket_utils.py
@@ -1,7 +1,9 @@
-# Read data from client and return when newline is received
+# Read data from the client and return all complete messages as a single string
 def receive_data(socket):
     # Variable to store received data
     data = b''
+    message_full = ''
+
     while True:
         # Receive data in chunks
         chunk = socket.recv(1024)
@@ -10,7 +12,13 @@ def receive_data(socket):
             raise ConnectionResetError
 
         data += chunk
-        if b'\n' in data:
-            # If we have a complete message (ending with '\n'), return it
+        while b'\n' in data:
+            # If we have a complete message (ending with '\n'), append it to the full message
             message, _, data = data.partition(b'\n')
-            return message.decode('utf-8').strip()
+            message_full += message.decode()
+
+        # Check if we have received all messages, i.e., no more '\n' in the remaining data
+        if b'\n' not in data:
+            break
+
+    return message_full.strip()
